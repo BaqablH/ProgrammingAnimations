cb7c128442249b791778a59e673dc071db7dd69f
Victor <victormartin96@outlook.com>
2021-05-02 18:05:29 +0200

    Added code

---
+++ projects/DFS/codes/code-anim.cc
@@ -0,0 +1,9 @@
+void explore(int y, int x) {
+    // Marks the square as not explorable
+    is_explorable[y][x] = false;
+    
+    // Explore explorable adjacent squares
+    for (int i : {0, 1, 2, 3})
+        if (is_explorable[y + dy[i]][x + dx[i])
+            explore(y + dy[i], x + dx[i]);
+}
